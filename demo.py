a55f585e5554cc16dfec80e9916abdece750c0da
syed kazim usman <56635461+kazimusman@users.noreply.github.com>
2022-07-05 02:02:24 +0500

    configuration.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+# Here we will write a code to see a demo
+from housing.pipeline.pipeline import Pipeline
+from housing.exception import HousingException
+from housing.logger import logging
+def main():
+    try:
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+
+
+if __name__=="__main__":
+    main()
--- housing/util/util.py
+++ housing/util/util.py
@@ -3,7 +3,6 @@
 # util is kind of helper function, so we are going to create few functions inside util and we 
 # will use these function whenever they are required 
 
-import sys
 import yaml
 from housing.exception import HousingException
 import os,sys
@@ -11,13 +10,12 @@ import os,sys
 # Where we are going to use this read_yaml_file function? we will use in configuration.py file 
 
 def read_yaml_file(file_path:str)->dict:
-
     """
     Reads a YAML file and returns the contents as a dictionary.
     file_path: str
     """
     try:
-        with open(file_path,'rb') as yaml_file:
+        with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
-        raise HousingException(e,sys) from e
+        raise HousingException(e,sys) from e 
