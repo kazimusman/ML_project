fa64c508b99e0813d9e2740c11de56a15bdb5e27
syed kazim usman <56635461+kazimusman@users.noreply.github.com>
2022-07-13 22:07:29 +0500

    data_validation.py updated

--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -1,133 +1,77 @@
-import sys,os
-from housing.exception import HousingException
+
+
+from tkinter import E
+from token import EXACT_TOKEN_TYPES
 from housing.logger import logging
+from housing.exception import HousingException
+from housing.entity.config_entity import DataValidationConfig
 from housing.entity.artifact_entity import DataIngestionArtifact
-import tarfile
-import numpy as np
-from six.moves import urllib
-import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit
+import os,sys
 
-class DataIngestion:
 
-    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+
+
+class DataValidation:
+    
+
+    def __init__(self,data_validaion_config:DataValidationConfig,
+        data_ingestion_artifact:DataIngestionArtifact):
         try:
-            logging.info(f"{'='*20}Data Ingestion log started.{'='*20} ")
-            self.data_ingestion_config = data_ingestion_config
-
-        except Exception as e:
-            raise HousingException(e,sys)
-
-
-    def download_housing_data(self,) -> str:
-        try:
-            #extraction remote url to download dataset
-            download_url = self.data_ingestion_config.dataset_download_url
-
-            #folder location to download file
-            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
-
-            if os.path.exists(tgz_download_dir):
-                os.remove(tgz_download_dir)
-
-            os.makedirs(tgz_download_dir,exist_ok=True)
-
-            housing_file_name = os.path.basename(download_url)
-
-            tgz_file_path = os.path.join(tgz_download_dir, housing_file_name)
-
-            logging.info(f"Downloading file from :[{download_url}] into :[{tgz_file_path}]")
-            urllib.request.urlretrieve(download_url, tgz_file_path)
-            logging.info(f"File :[{tgz_file_path}] has been downloaded successfully.")
-            return tgz_file_path
-
-        except Exception as e:
-            raise HousingException(e,sys) from e
-
-    def extract_tgz_file(self,tgz_file_path:str):
-        try:
-            raw_data_dir = self.data_ingestion_config.raw_data_dir
-
-            if os.path.exists(raw_data_dir):
-                os.remove(raw_data_dir)
-
-            os.makedirs(raw_data_dir,exist_ok=True)
-
-            logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
-            with tarfile.open(tgz_file_path) as housing_tgz_file_obj:
-                housing_tgz_file_obj.extractall(path=raw_data_dir)
-            logging.info(f"Extraction completed")
-
-        except Exception as e:
-            raise HousingException(e,sys) from e
-
-    def split_data_as_train_test(self) -> DataIngestionArtifact:
-        try:
-            raw_data_dir = self.data_ingestion_config.raw_data_dir
-
-            file_name = os.listdir(raw_data_dir)[0]
-
-            housing_file_path = os.path.join(raw_data_dir,file_name)
-
-
-            logging.info(f"Reading csv file: [{housing_file_path}]")
-            housing_data_frame = pd.read_csv(housing_file_path)
-
-            housing_data_frame["income_cat"] = pd.cut(
-                housing_data_frame["median_income"],
-                bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
-                labels=[1,2,3,4,5]
-            )
-
-
-            logging.info(f"Splitting data into train and test")
-            strat_train_set = None
-            strat_test_set = None
-
-            split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-
-            for train_index,test_index in split.split(housing_data_frame, housing_data_frame["income_cat"]):
-                strat_train_set = housing_data_frame.loc[train_index].drop(["income_cat"],axis=1)
-                strat_test_set = housing_data_frame.loc[test_index].drop(["income_cat"],axis=1)
-
-            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
-                                            file_name)
-
-            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
-                                        file_name)
-
-            if strat_train_set is not None:
-                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
-                logging.info(f"Exporting training datset to file: [{train_file_path}]")
-                strat_train_set.to_csv(train_file_path,index=False)
-
-            if strat_test_set is not None:
-                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
-                logging.info(f"Exporting test dataset to file: [{test_file_path}]")
-                strat_test_set.to_csv(test_file_path,index=False)
-
-
-            data_ingestion_artifact = DataIngestionArtifact(train_file_path=train_file_path,
-                                test_file_path=test_file_path,
-                                is_ingested=True,
-                                message=f"Data ingestion completed successfully."
-                                )
-            logging.info(f"Data Ingestion artifact:[{data_ingestion_artifact}]")
-            return data_ingestion_artifact
-
-        except Exception as e:
-            raise HousingException(e,sys) from e
-
-    def initiate_data_ingestion(self)-> DataIngestionArtifact:
-        try:
-            pass
-            tgz_file_path =  self.download_housing_data()
-            self.extract_tgz_file(tgz_file_path=tgz_file_path)
-            return self.split_data_as_train_test()
+            self.data_validation_config = data_validaion_config
+            self.data_ingestion_artifact = data_ingestion_artifact
         except Exception as e:
             raise HousingException(e,sys) from e
 
 
+    def is_train_test_file_exists(self)->bool:
+        try:
+            logging.info("checking if training and test file is available")
+            is_train_file_exist = False
+            is_test_file_exist = False
 
-    def __del__(self):
-        logging.info(f"{'='*20}Data Ingestion log completed.{'='*20} \n\n")
+            train_file_path = self.data_ingestion_artifact.train_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+
+            is_train_file_exist = os.path.exists(train_file_path)
+            is_test_file_exist = os.path.exists(test_file_path)
+
+            is_available = is_train_file_exist and is_test_file_exist
+
+            logging.info(f"Is train and test file exists?-> {is_available}")
+
+            if not is_available:
+                training_file = self.data_ingestion_artifact.train_file_path
+                testing_file = self.data_ingestion_artifact.test_file_path
+                message = f"Training file: {training_file} or Testing_file: {testing_file}" \
+                    "is not present"
+
+                raise Exception(message)
+
+            return is_available
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+
+    def validate_dataset_schema(self)->bool:
+        try:
+            validation_status = False
+
+            # Assignment validate training and testing dataset using schema file 
+            # 1. Number of column 
+            # 2. check the value of ocean proximity 
+            # acceptable values : <1H OCEAN, INLAND, ISLAND, NEAR BAY, NEAR OCEAN, 
+            # 3. check column names 
+
+            validation_status = True
+            return validation_status
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+    
+    def initiate_data_validation(self):
+        try:
+            self.is_train_test_file_exists()
+            self.validate_dataset_schema()
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
